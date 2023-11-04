b0826141561ba41602a3ac380752493956679814
Michael Wellner <michael.wellner@gmail.com>
2023-11-04 23:00:31 +0100

    feat: Separate logic for work lights

--- src/main/java/com/wellnr/home/services/AutomaticLightningService.java
+++ src/main/java/com/wellnr/home/services/AutomaticLightningService.java
@@ -2,6 +2,7 @@ package com.wellnr.home.services;
 
 import com.luckycatlabs.sunrisesunset.SunriseSunsetCalculator;
 import com.luckycatlabs.sunrisesunset.dto.Location;
+import com.wellnr.common.Operators;
 import com.wellnr.common.markup.Tuple3;
 import com.wellnr.home.framework.EWeLinkSwitch;
 import com.wellnr.home.framework.TasmatoPlug;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.Objects;
 import java.util.TimeZone;
@@ -28,28 +30,34 @@ public class AutomaticLightningService {
 
     private final SunriseSunsetCalculator sunriseSunsetCalculator;
 
-    Boolean lightsTurnedOn;
+    Boolean nightLightsTurnedOn;
+
+    Boolean workLightsTurnedOn;
 
     public AutomaticLightningService(IMqttClient client, EWeLinkSwitch outsideSwitch) {
-        this.iotPlugs = new TasmatoPlug[5];
+        this.iotPlugs = new TasmatoPlug[6];
 
-        this.iotPlugs[0] = new TasmatoPlug(client, "wellnr/home", "iot-plug-001");
-        this.iotPlugs[1] = new TasmatoPlug(client, "wellnr/home", "iot-plug-002");
-        this.iotPlugs[2] = new TasmatoPlug(client, "wellnr/home", "iot-plug-003");
-        this.iotPlugs[3] = new TasmatoPlug(client, "wellnr/home", "iot-plug-004");
-        this.iotPlugs[4] = new TasmatoPlug(client, "wellnr/home", "iot-plug-005");
+        this.iotPlugs[0] = new TasmatoPlug(client, "wellnr/home", "iot-plug-001"); // Gallery
+        this.iotPlugs[1] = new TasmatoPlug(client, "wellnr/home", "iot-plug-002"); // Living-Room
+        this.iotPlugs[2] = new TasmatoPlug(client, "wellnr/home", "iot-plug-003"); // Entrance/ Typewriter
+        this.iotPlugs[3] = new TasmatoPlug(client, "wellnr/home", "iot-plug-004"); // Work window
+        this.iotPlugs[4] = new TasmatoPlug(client, "wellnr/home", "iot-plug-005"); // Work window
+        this.iotPlugs[5] = new TasmatoPlug(client, "wellnr/home", "iot-plug-006"); // Work wall
 
         this.outsideSwitch = outsideSwitch;
 
         var location = new Location("49.442009", "6.636030");
         this.timeZone = TimeZone.getTimeZone("Europe/Berlin");
         this.sunriseSunsetCalculator = new SunriseSunsetCalculator(location, timeZone);
-        this.lightsTurnedOn = null;
+        this.nightLightsTurnedOn = null;
+        this.workLightsTurnedOn = null;
     }
 
     @Scheduled(fixedRate = 30_000)
     public void switchLights() {
         var nextSunriseAndNextSunset = this.getNextSunriseAndNextSunset();
+
+        var shutdownWorkLightsTime = LocalTime.parse("01:00:00");
 
         var today = nextSunriseAndNextSunset.get_3().toLocalDate();
         var sunrise = nextSunriseAndNextSunset.get_1().toLocalDate();
@@ -57,39 +65,68 @@ public class AutomaticLightningService {
         var sunriseIsToday = today.isEqual(sunrise);
         var sunsetIsToday = today.isEqual(sunset);
 
-        if (sunriseIsToday && sunsetIsToday) {
-            turnOn();
-        } else if (!sunriseIsToday && sunsetIsToday) {
-            turnOff();
-        } else {
-            turnOn();
+        if (sunriseIsToday && sunsetIsToday) { // after midnight
+            Operators.ignoreExceptions(this::turnOnNightLights, log);
+
+            if (LocalTime.now().isBefore(shutdownWorkLightsTime)) {
+                Operators.ignoreExceptions(this::turnOnWorkLights, log);
+            } else {
+                Operators.ignoreExceptions(this::turnOffWorkLights, log);
+            }
+        } else if (!sunriseIsToday && sunsetIsToday) { // during daylight
+            Operators.ignoreExceptions(this::turnOffNightLights, log);
+            Operators.ignoreExceptions(this::turnOffWorkLights, log);
+        } else { // after sunset
+            Operators.ignoreExceptions(this::turnOnNightLights, log);
+            Operators.ignoreExceptions(this::turnOnWorkLights, log);
         }
     }
 
-    private void turnOn() {
-        if (Objects.isNull(this.lightsTurnedOn) || !this.lightsTurnedOn) {
+    private void turnOnNightLights() {
+        if (Objects.isNull(this.nightLightsTurnedOn) || !this.nightLightsTurnedOn) {
             log.info("Send request to turn on the lights ...");
-            for (var i = 0; i < 5; i++) {
+            for (var i = 0; i < 3; i++) {
                 this.iotPlugs[i].turnOn();
             }
 
             this.outsideSwitch.turnOn();
-
-            this.lightsTurnedOn = true;
+            this.nightLightsTurnedOn = true;
         }
     }
 
-    private void turnOff() {
-        if (Objects.isNull(this.lightsTurnedOn) || this.lightsTurnedOn) {
+    private void turnOnWorkLights() {
+        if (Objects.isNull(this.workLightsTurnedOn) || !this.workLightsTurnedOn) {
+            log.info("Send request to turn on work lights");
+            for (var i = 3; i < 6; i++) {
+                this.iotPlugs[i].turnOn();
+            }
+
+            this.workLightsTurnedOn = true;
+        }
+    }
+
+    private void turnOffNightLights() {
+        if (Objects.isNull(this.nightLightsTurnedOn) || this.nightLightsTurnedOn) {
             log.info("Send request to turn off the lights ...");
 
-            for (var i = 0; i < 5; i++) {
+            for (var i = 0; i < 3; i++) {
                 this.iotPlugs[i].turnOff();
             }
 
             this.outsideSwitch.turnOff();
+            this.nightLightsTurnedOn = false;
+        }
+    }
 
-            this.lightsTurnedOn = false;
+    private void turnOffWorkLights() {
+        if (Objects.isNull(this.workLightsTurnedOn) || this.workLightsTurnedOn) {
+            log.info("Send request to turn off work lights ...");
+
+            for (var i = 3; i <6; i++) {
+                this.iotPlugs[i].turnOff();
+            }
+
+            this.workLightsTurnedOn = false;
         }
     }
 
