ece425baaad22fda7031032936fc187377d15f16
Michael Wellner <michael.wellner@gmail.com>
2024-12-05 22:30:26 +0100

    So my boss wanted this button ...

--- src/main/java/com/wellnr/home/ports/weather/BrightSightWeatherAPIPort.java
+++ src/main/java/com/wellnr/home/ports/weather/BrightSightWeatherAPIPort.java
@@ -2,11 +2,14 @@ package com.wellnr.home.ports.weather;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
 import java.util.Objects;
 
+@Slf4j
 @AllArgsConstructor
 public final class BrightSightWeatherAPIPort implements WeatherPort {
 
@@ -30,8 +33,10 @@ public final class BrightSightWeatherAPIPort implements WeatherPort {
 
         try (var response = client.newCall(request).execute()) {
             var json = Objects.requireNonNull(response.body(), "Expected response JSON in body.").string();
-
             var resp = om.readValue(json, CurrentWeatherResponse.class);
+
+            log.info("Fetched weather data: {}", resp);
+
             return resp.getWeather();
         } catch (Exception e) {
             throw new RuntimeException("Failed to fetch weather data", e);
